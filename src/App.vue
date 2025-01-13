0323c11fd4ebcaa355705f984029de3b0e7f58d8
ViktorSkomorokhov <sladkiysous@gmail.com>
2025-01-13 19:25:12 +0300

    добавил таблицу удалил карточки =)

--- src/App.vue
+++ src/App.vue
@@ -13,34 +13,38 @@ const { timers } = useTimerStore()
 const formatTime = (time?: number | string) => {
   return dayjs(time).format('HH:mm:ss')
 }
+
+const formatDays = (time:string) => {
+  return dayjs(time).format('D.MM.YYYY')
+}
 </script>
 
 <template>
   <div class="flex justify-center items-center h-screen flex-col gap-4">
     <MainTimer />
     <div>
-      <DataTable tableStyle="min-width: 50rem">
-        <Column field="code" header="Code"></Column>
-        <Column field="name" header="Name"></Column>
-        <Column field="category" header="Category"></Column>
-        <Column field="quantity" header="Quantity"></Column>
+      <DataTable :value="timers"  paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"  scrollable scrollHeight="400px" tableStyle="min-width: 50rem">
+        <Column field="id" header="Code"></Column>
+        <Column field="title" header="Name"></Column>
+        <Column field="start" header="Category">
+          <template #body="slotProps">
+            <span> {{ formatDays(slotProps.data.start) }}</span>
+        </template>
+      </Column>
       </DataTable>
     </div>
-    <div v-if="timers.length" class="grid grid-cols-4 md:grid-cols-6 gap-4">
+    <!-- <div v-if="timers.length" class="grid grid-cols-4 md:grid-cols-6 gap-4">
       <Card v-for="timer in timers" :key="timer.id">
         <template #title>
-          <InputText
-            v-model="timer.title"
-            :placeholder="`Таймер номер ${timer.id}`"
-            class="!bg-transparent !border-0 !p-0"
-          />
+          <InputText v-model="timer.title" :placeholder="`Таймер номер ${timer.id}`"
+            class="!bg-transparent !border-0 !p-0" />
         </template>
         <template #content>
           <p class="m-0">Время запуска:{{ formatTime(timer.start) }}</p>
           <p v-if="timer.end" class="m-0">Время окончания:{{ formatTime(timer.end) }}</p>
         </template>
       </Card>
-    </div>
+    </div> -->
     <StickyTimer />
   </div>
 </template>
