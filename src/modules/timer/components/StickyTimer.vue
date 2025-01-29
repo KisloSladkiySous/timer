55cfecc8b32492f57dd1ee6247c51b034f07dec5
Zazik <zaza.haratishvili@gmail.com>
2025-01-29 23:40:50 +0300

    pretty timers

--- src/modules/timer/components/StickyTimer.vue
+++ src/modules/timer/components/StickyTimer.vue
@@ -3,14 +3,18 @@ import { ref, watch } from 'vue'
 
 import { useTimerStore } from '@/modules/timer/store/useTimerStore'
 import PlayButton from './PlayButton.vue'
-const { activeTimeEntry, stopTimer, startTimer, currentTimer } = useTimerStore()
+const { activeTimeEntry, startTimer, currentTimer } = useTimerStore()
 
-const time = ref()
+import NumberFlow, { NumberFlowGroup } from '@number-flow/vue'
+
+const hours = ref<number>(0)
+const minutes = ref<number>(0)
+const seconds = ref<number>(0)
 const getTimeFormatted = (duration: number) => {
-  const hours = Math.floor(duration / 3600)
-  const minutes = Math.floor((duration % 3600) / 60)
-  const seconds = Math.floor(duration % 60)
-  time.value = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  hours.value = Math.floor(duration / 3600)
+  minutes.value = Math.floor((duration % 3600) / 60)
+  seconds.value = Math.floor(duration % 60)
+  // time.value = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
 watch(
@@ -33,8 +37,30 @@ const start = () => {
   >
     <div class="flex gap-4 items-center justify-between transition-[width] duration-300">
       <span class="mr-12" v-if="currentTimer">{{ currentTimer.title }}</span>
-      <span v-if="time">{{ time }}</span>
+
+      <NumberFlowGroup>
+        <div
+          style="font-variant-numeric: tabular-nums; --number-flow-char-height: 0.85em"
+          class="~text-3xl/4xl flex items-baseline font-semibold"
+        >
+          <NumberFlow :value="hours" :format="{ minimumIntegerDigits: 2 }" />
+          <NumberFlow
+            prefix=":"
+            :value="minutes"
+            :digits="{ 1: { max: 5 } }"
+            :format="{ minimumIntegerDigits: 2 }"
+          />
+          <NumberFlow
+            prefix=":"
+            :value="seconds"
+            :digits="{ 1: { max: 5 } }"
+            :format="{ minimumIntegerDigits: 2 }"
+          />
+        </div>
+      </NumberFlowGroup>
+      <!-- <span v-if="time">{{ time }}</span> -->
       <PlayButton :active="!!activeTimeEntry.interval" @click="start" />
     </div>
   </div>
 </template>
+<style scoped></style>
